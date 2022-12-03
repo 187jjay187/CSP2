7d8ab34e33a439be59fe22b4de671bcf50c463f5
jorgegoco <jorgegoco70@gmail.com>
2022-12-03 15:43:55 +0100

    Remove inline comments of used code

--- src/modules/displaymeals.js
+++ src/modules/displaymeals.js
@@ -4,14 +4,14 @@ const mealListDisplay = (meal) => {
   const div = document.createElement('div');
   // div section for meal card and info
   div.setAttribute('data-id', meal.idCategory);
-  // div.innerHTML = `
-  // <div class="meal-card">
-  // <div class="img-of-meal"><img src="${meal.strCategoryThumb}" alt="meal img"></div>
-  // <div class="meal-title-name">
-  // <div class="meal-title"><h3>${meal.strCategory}</h3></div>
-  // <div class="likes-display"><i class="uil uil-heart love-icon"></i><p>Likes</p></div>
-  // </div>
-  // <button type="button" class="comment-Button" id="comment-button">Comment</button></div>`;
+  div.innerHTML = `
+  <div class="meal-card">
+  <div class="img-of-meal"><img src="${meal.strCategoryThumb}" alt="meal img"></div>
+  <div class="meal-title-name">
+  <div class="meal-title"><h3>${meal.strCategory}</h3></div>
+  <div class="likes-display"><i class="uil uil-heart love-icon"></i><p>Likes</p></div>
+  </div>
+  <button type="button" class="comment-Button" id="comment-button">Comment</button></div>`;
 
   container.appendChild(div);
 };
